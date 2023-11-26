e4b325d670475086515e355b2e2b7556c7d50d93
Hernan Amiune <amiune@gmail.com>
2023-11-26 13:54:00 +0100

    Bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,20 @@
 var modal = document.getElementById('videoModal');
 // Get the iframe within the modal
 var iframe = modal.querySelector('iframe');
-// Get the close button
-var span = document.getElementsByClassName("close")[0];
 // Get all video buttons
 var videoButtons = document.querySelectorAll('.video-btn');
 
 // When a video button is clicked, open the modal and load the video
 videoButtons.forEach(function(btn){
-btn.addEventListener('click', function(){
-    var videoId = btn.getAttribute('data-video-id');
-    iframe.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0';
-    modal.style.display = 'block';
-});
+    btn.addEventListener('click', function(){
+        var videoId = btn.getAttribute('data-video-id');
+        iframe.src = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1&rel=0';
+        modal.style.display = 'block';
+    });
 });
 
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', function(event) {
-if (event.target == modal) {
+// When the user clicks anywhere in the modal outside the video, close it
+modal.addEventListener('click', function(event) {
     modal.style.display = "none";
     iframe.src = ''; // Stop the video by clearing the src
-}
 });
